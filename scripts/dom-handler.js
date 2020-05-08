b0d9ab0daaa4cd5374d00f7ba5770c704d766c11
Katarina Svorcan <kata_che@hotmail.com>
2020-05-08 15:50:45 -0700

    Uncover all empty cells (non-bomb cells) - work in progress

--- scripts/dom-handler.js
+++ scripts/dom-handler.js
@@ -4,6 +4,12 @@ const CLASSES = {
   GRID: 'minesweeper-grid',
 };
 
+const CELL_CONTENTS = {
+  BOMB: 1,
+  EMPTY: 0,
+  VISITED: 9,
+};
+
 class DomHandler {
   game = null;
   gameRootElement = document.getElementById('minesweeper-app');
@@ -11,7 +17,8 @@ class DomHandler {
   constructor(game) {
     this.game = game;
     this._addGridToDom();
-    this._setUpEventHandler();
+    this._setUpEventHandlers();
+    console.log(this.game);
   }
 
   _addGridToDom() {
@@ -35,13 +42,51 @@ class DomHandler {
     this.gameRootElement.appendChild(tableEl);
   }
 
-  _setUpEventHandler() {
-    this.gameRootElement.addEventListener('click', (event) => {
-      if (!event.target.classList.contains(CLASSES.CELL)) {
+  _setUpEventHandlers() {
+    this.gameRootElement.addEventListener('mouseup', (event) => {
+      const clickedElement = event.target;
+      if (!clickedElement.classList.contains(CLASSES.CELL)) {
         return;
       }
 
-      console.log('clicked on a cell!');
+      const { row, col } = clickedElement.dataset;
+      const { grid } = this.game;
+
+      if (grid[row][col] === CELL_CONTENTS.BOMB) {
+        clickedElement.style.backgroundImage = `url(${icons.explodedBomb})`;
+      } else if (grid[row][col] === CELL_CONTENTS.EMPTY) {
+        this._uncoverAllContinuesBlankCells(row, col);
+      } else {
+        clickedElement.style.backgroundImage = `url(${icons.blank})`;
+      }
     });
+
+    this.gameRootElement.addEventListener('mousedown', (event) => {
+      const clickedElement = event.target;
+      if (!clickedElement.classList.contains(CLASSES.CELL)) {
+        return;
+      }
+      clickedElement.style.backgroundImage = `url(${icons.pressed})`;
+    });
+  }
+
+  _uncoverAllContinuousBlankCells(row, col) {
+    const { grid } = this.game;
+
+    const DFS = (row, col) => {
+      console.log(row, col);
+      const currentCell = grid[row] ? grid[row][col] : undefined;
+      if (currentCell === CELL_CONTENTS.EMPTY) {
+        grid[row][col] = CELL_CONTENTS.VISITED;
+        const cellEl = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+        cellEl.classList.add('opened');
+
+        DFS(row - 1, col); // up
+        DFS(row, col + 1); // right
+        DFS(row + 1, col); // down
+        DFS(row, col - 1); // left
+      }
+    };
+    DFS(Number(row), Number(col));
   }
 }
